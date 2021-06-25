fb23ad4ed5ff5271ec6fe4262359435ecfbceff6
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-25 09:02:26 +0530

    [mihir] Added: createTicTacToeBoard function which insert - in tictactoeArray which is of char type ignoring index 0 starting from 1 to the last positionusing for loop for insertion in array.

---
+++ src/com/bridgelab/tictactoe/TicTacToe.java
@@ -0,0 +1,36 @@
+package com.bridgelab.tictactoe;
+
+/**********************************************
+ * @author mihir
+ * 
+ * @since 25-June-2021
+ * 
+ * In this class we are creating TicTacToe game.
+ **********************************************/
+public class TicTacToe
+{
+	public static char[] tictactoeArray = new char[10];
+	
+	/**
+	 * Name : createTicTacToeBoard.
+	 * 
+	 * Description : Creating Tic-Tac-Toe Board.
+	 * 
+	 * Algorithm : In this function we are taking an char[] tictactoeArray array and starting from the 1st index 
+	 * Assigning '-' till the last index number.
+	 * 
+	 * Modified : First commit 25-June-2021
+	 */
+	public static void createTicTacToeBoard()
+	{
+		for(int i = 1; i <= tictactoeArray.length; i++)
+		{
+			tictactoeArray[i] = '-';
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		createTicTacToeBoard();
+	}
+}
